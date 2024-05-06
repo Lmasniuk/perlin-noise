c6dfa97d3e7b4876a734b474637408812df70aef
Logan Masniuk <logan.masniuk@gmail.com>
2024-05-06 15:05:24 -0700

    Move a dot around the canvas via perlin noise generation

--- p5.js
+++ p5.js
@@ -1,7 +1,15 @@
+let xoff = 0
+
 function setup() {
     createCanvas(400, 400);
   }
   
   function draw() {
     background(52, 235, 192);
+
+    const x = map(noise(xoff), 0 , 1, 0, width)
+
+    ellipse(x,200,24,24)
+
+    xoff += 0.02
   }
